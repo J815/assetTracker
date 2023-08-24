6667694d6cb21d5b7026dfd4e4376fa15a0e06dd
jubayer <jubayermd327@gmail.com>
2023-08-25 01:42:23 +0600

    2. model creation and register

--- asset_tracking/admin.py
+++ asset_tracking/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Company,Employee,DeviceType,Device,DeviceLog
 # Register your models here.
+admin.site.register(Company)
+admin.site.register(DeviceLog)
+admin.site.register(Device)
+admin.site.register(DeviceType)
+admin.site.register(Employee)
--- asset_tracking/models.py
+++ asset_tracking/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# for different companies
+class Company(models.Model):
+    name = models.CharField(max_length=100)  
+    
+# employee and the company he works
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+class DeviceType(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Device(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    device_type = models.ForeignKey(DeviceType, on_delete=models.CASCADE)
+
+class DeviceLog(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    checkout_date = models.DateTimeField()
+    return_date = models.DateTimeField(null=True, blank=True)
+    condition_on_checkout = models.TextField()
+    condition_on_return = models.TextField(null=True, blank=True)
